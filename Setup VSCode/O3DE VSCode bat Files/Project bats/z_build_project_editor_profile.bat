639dc9b20a307e39cc7b3897f49eefdae0cdc702
GaianHelmers <gaianhelmers@genomestudios.ca>
2025-09-09 09:44:20 -0600

    Project Build now checks for Editor.exe before building!

--- Setup VSCode/O3DE VSCode bat Files/Project bats/z_build_project_editor_profile.bat
+++ Setup VSCode/O3DE VSCode bat Files/Project bats/z_build_project_editor_profile.bat
@@ -4,6 +4,21 @@ echo === Setting up Visual Studio environment ===
 REM Path to your Visual Studio 2022 installation (adjust if needed)
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
+REM === Check if O3DE Editor is running ===
+set "processName=Editor.exe"
+
+tasklist /FI "IMAGENAME eq %processName%" | find /I "%processName%" >nul
+if not errorlevel 1 (
+    echo.
+    echo WARNING: %processName% is currently running.
+    echo Please save your work. By continuing it will be closed.
+    pause
+    echo Closing %processName%...
+    taskkill /F /IM %processName%
+    echo %processName% has been closed.
+    echo.
+)
+
 echo === Building Project ===
 cmake --build build/windows --target Editor --config profile
 pause
